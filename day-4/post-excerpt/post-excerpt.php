c7eb6f8feaff9b5f15100a2b4fec92c19d57588f
moshiur rahman <moshiurcse888@gmail.com>
2021-03-09 18:02:39 +0600

    assinment day four added

---
+++ day-4/post-excerpt/post-excerpt.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Plugin Name:       Post Excerpt
+ * Plugin URI:        https://post-xcerpt.com
+ * Description:       Post Excerpt with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Moshiur Rahman
+ * Author URI:        https://moshiurrahman.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       post-excerpt
+ * Domain Path:       /languages
+ */
+
+/**
+ * Exit if accessed directly
+ */ 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * The main class of title capitalize plugin 
+ */
+class Mrm_Post_Excerpt {
+
+    /**
+     * Constructor function
+     *
+     * @return miexd
+     */
+    function __construct() {
+        add_action( 'add_meta_boxes', [ $this , 'mrm_add_metabox' ] );
+        add_action( 'save_post', [ $this, 'mrm_save_post_meta' ] );
+        /**
+         * Post count excerpt shortcode
+         */
+        add_shortcode( 'mrm-post-count', [ $this, 'mrm_latest_post_count' ] );
+    }
+    
+    /**
+     * Meta box add function
+     *
+     * @param string $post_type
+     * 
+     * @return void
+     */
+    public function mrm_add_metabox( $post_type ) {
+        add_meta_box( 
+            'textarea_excerpt_metabox',
+            'Excerpt Under Post',
+            [ $this, 'mrm_textarea_excerpt_metabox'],
+            'post'
+        );
+    }
+    
+    /**
+     * Show meta box function
+     *
+     * @param int $post
+     * 
+     * @return void
+     */
+    public function mrm_textarea_excerpt_metabox( $posts ) {
+        $post_excerpt = get_post_meta( $posts->ID, 'excerpt_under_post', true );
+        $description    = ! empty( $post_excerpt['description'] ) ?  $post_excerpt['description'] : '';
+        ?>
+        <?php wp_nonce_field( 'mrm_textarea_excerpt_metabox', 'textarea_excerpt_metabox_nonce' ); ?>
+            <div class="form-group">
+                <label for="Textarea1"><?php esc_html_e( 'Excerpt Post', 'post-excerpt' ) ?></label>
+                <textarea class="form-control widefat id="Textarea1" name="description" rows="3"><?php echo esc_html_e( $description ); ?></textarea>
+            </div>
+        <?php
+    }
+
+    /**
+     * Save post Meta function
+     *
+     * @param int $post_id
+     * 
+     * @return object
+     */
+    public function mrm_save_post_meta( $post_id ) {
+        $nonce = isset( $_POST[ 'textarea_excerpt_metabox_nonce' ] ) ? $_POST[ 'textarea_excerpt_metabox_nonce' ] : '';
+        $description = isset( $_POST['description'] ) ? sanitize_text_field( $_POST['description'] ) : '';
+
+        if ( ! wp_verify_nonce( $nonce, 'mrm_textarea_excerpt_metabox' ) ) {
+            return;
+        }
+
+        $post_excerpt_update = [
+            'description'   => $description
+        ];
+        update_post_meta( $post_id, 'excerpt_under_post', $post_excerpt_update );
+    }
+
+    /**
+     * Post Count sortcode function
+     *
+     * @param array $atts
+     * 
+     * @return string
+     */
+    public function mrm_latest_post_count( $atts, $content ) {
+         $atts = shortcode_atts( 
+            array (
+            'id'       => '',
+            'category' => '',
+            'post_no'  => '10',
+        ), $atts );
+
+        /* Wp query argument set */
+        $args = array(
+            'post_type'      => 'post', 
+            'meta_key'       => 'excerpt_under_post', 
+            'posts_per_page' => $atts['post_no'],
+            'order'          => 'DESC',
+            'orderby'        => 'excerpt_under_post',
+            'post_status'    => 'publish'
+        );
+
+          /* Check category Name*/
+          if ( $atts['category'] != '' ) {
+            // print_r($category_name);exit();
+            $args['category_name'] = $atts['category'];
+        }
+
+        /* Check post id */
+        if ( $atts['id']  != '' ) {
+            $post_id = explode( ',', $atts['id'] );
+            $args['post__in'] = $post_id;
+            unset( $args['category_name'] );
+        }
+
+        /* Main Query */
+        $query = new \WP_Query( $args );
+        while ( $query->have_posts() ) {
+            $query->the_post();
+            echo '<li>' . $query->post->excerpt_under_post['description'] . '</li>';
+        }
+
+        /* Restore original Post Data */
+        wp_reset_postdata();
+
+        return $content;
+    }
+}
+
+/**
+ * The main class instance create function
+ *
+ * @return object
+ */
+function mrm_post_excerpt() {
+    return new Mrm_Post_Excerpt();
+}
+
+/**
+ * object calling function
+ */
+mrm_post_excerpt();
---
+++ day-4/posts-view/posts-view.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Plugin Name:       Posts View
+ * Plugin URI:        https://posts-view.com
+ * Description:       Posts view with this plugin.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Moshiur Rahman
+ * Author URI:        https://moshiur-rahman.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       posts-view
+ * Domain Path:       /languages
+ */
+
+/**
+ * Exit if accessed directly
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * The main class of posts view plugin
+ */
+class Posts_View_Count {
+
+    /**
+     * Constructor Function
+     */
+    public function __construct()
+    {
+        add_filter( 'the_content', [ $this, 'posts_view_count' ] );
+          /**
+         * Post view & title show shortcode
+         */
+        add_shortcode( 'post_count_view', [ $this, 'mrm_post_count_view' ] );
+    }
+
+    /**
+     * The main function post view count
+     *
+     * @param string $content
+     * 
+     * @return array
+     */
+    public function posts_view_count( $content )
+    {
+        if ( is_single() ) {
+            $post_id = get_the_ID();
+            $post_view = get_post_meta( $post_id, 'view_post_single_page', true );
+            $views = isset( $post_view ) ? ( int ) $post_view + 1 : 0;
+            $count = update_post_meta( $post_id, 'view_post_single_page', $views );
+            return $content . '<h2>View Post : ' . apply_filters( 'modify_count_view', $views ) . '</h2>';
+        } else {
+            return $content;
+        }
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param array $atts
+     * 
+     * @return string
+     */
+    public function mrm_post_count_view( $atts, $content ) {
+        $atts = shortcode_atts( 
+           array (
+           'id'       => '',
+           'category' => '',
+           'post_no'  => '10',
+           'order'    => 'DESC',
+       ), $atts );
+
+       /* Wp query argument set */
+       $args = array(
+           'post_type'      => 'post', 
+           'meta_key'       => 'view_post_single_page', 
+           'posts_per_page' => $atts['post_no'],
+           'order'          => $atts['order'],
+           'orderby'        => 'meta_value_num',
+           'post_status'    => 'publish'
+       );
+
+         /* Check category Name*/
+         if ( $atts['category'] != '' ) {
+           $args['category_name'] = $atts['category'];
+       }
+
+       /* Check post id */
+       if ( $atts['id']  != '' ) {
+           $post_id = explode( ',', $atts['id'] );
+           $args['post__in'] = $post_id;
+           unset( $args['category_name'] );
+       }
+
+       /* Main Query */
+       $query = new \WP_Query( $args );
+       while ( $query->have_posts() ) {
+           $query->the_post();
+           echo '<li>' . get_the_title() . ' ( ' . $query->post->view_post_single_page . ' ) ' . '</li>';
+       }
+
+       /* Restore original Post Data */
+       wp_reset_postdata();
+
+       return $content;
+    }
+}
+
+/**
+ * The main class instance
+ *
+ * @return object
+ */
+function posts_view()
+{
+    return new Posts_View_Count();
+}
+
+/**
+ * object calling function
+ */
+posts_view();
